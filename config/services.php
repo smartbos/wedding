ef3cfacadaba473bf298206b36816bc0bdd8f4ce
LeeHyunseok <smartbosslee@gmail.com>
2015-10-22 01:16:28 +0900

    facebook callback url 변경

--- config/services.php
+++ config/services.php
@@ -38,6 +38,6 @@ return [
     'facebook' => [
         'client_id' => '361562150596021',
         'client_secret' => '8d2013e71a68dd9e371c21b1d10ef6f2',
-        'redirect' => 'http://localwedding.leehyunseok.com/fb_callback',
+        'redirect' => 'http://wedding.leehyunseok.com/fb_callback',
     ],
 ];
