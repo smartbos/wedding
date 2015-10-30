0cf70e43481152ae61fb0ae3ee5426b71f8edf65
LeeHyunseok <smartbosslee@gmail.com>
2015-10-30 09:20:39 +0900

    fb

--- resources/views/invite.blade.php
+++ resources/views/invite.blade.php
@@ -230,6 +230,40 @@
     </div>
 </section>
 
+<hr>
+
+<section id="match" class="content-section text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Solo?</h2>
+                <p>
+                    신랑신부 지인들 참 좋은데, 정말 좋은데, 뭐라고 설명할 방법이 없네. <br>
+                </p>
+                <p>
+                    @if(!Auth::check())
+                        @if($user && $user->want_match)
+                            <a href="/matchOut" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-lw"></i> 신청취소</a>
+                        @else
+                            <a href="/matchIn" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-lw"></i> 소개팅인듯 소개팅아닌 소개팅같은거 <br/>페이스북으로 신청</a>
+                            <br />
+                            <span style="color:#aaaaaa; font-size:16px;">못해줄 수도 있습니다...<br> 페이스북에 게시하지 않습니다</span>
+                        @endif
+                    @else
+                        @if($user && $user->want_match)
+                            <a href="/matchOut" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-lw"></i> 신청취소</a>
+                        @else
+                            <a href="/matchIn" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-lw"></i> 소개팅인듯 소개팅아닌 <br/>소개팅같은거 신청</a>
+                            <br />
+                            <span style="color:#aaaaaa; font-size:16px;">못해줄 수도 있습니다...</span>
+                        @endif
+                    @endif
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
 <!-- jQuery -->
 <script src="/startbootstrap-grayscale-1.0.4/js/jquery.js"></script>
 
